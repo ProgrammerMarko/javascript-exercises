ffdca79cab5ce2f440ade19f587adbd42c5f8d9c
Marko <programmermarko1@gmail.com>
2025-11-24 21:54:11 +0100

    Add solution for 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
 
+  let result = 0;
+
+  if (num1 > num2) {
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+
+  for (num1; num1 <= num2; num1++) {
+    result += num1;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
